46b69820bf80dd695c40ab6c479076b639d3f7a4
Bartłomiej Strama <kontakt@bstrama.com>
2023-10-16 16:07:42 +0200

    package test added

---
+++ src/api/package_test.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestPackageMatrix(t *testing.T) {
+	testPackages := []string{
+		"pacman",
+		// Commented for test development speed
+		//"curl",
+		//"bash",
+	}
+	
+	for _, packageName := range testPackages {
+		testPackageInfo(packageName, t)
+	}
+}
+
+func testPackageInfo(packageName string, t *testing.T) {
+	// TODO: complete this function
+	p := Package{}
+	p.Get(packageName)
+
+	if p.Name == "" {
+		t.Fatalf("Couldn't resolve \"%s\" package name", packageName)
+	}
+	
+	if p.Version == "" {
+		t.Fatalf("Couldn't resolve \"%s\" package version", packageName)
+	}
+
+	if p.InstallDate == "" {
+		t.Fatalf("Couldn't resolve \"%s\" package install date", packageName)
+	}
+}
