9b3236f5d29939100ec5556547273ddebcb6bd5e
Bartłomiej Strama <80683535+BaderBC@users.noreply.github.com>
2023-10-19 11:04:02 +0200

    Systemd in docker tests (#15)
    - docker runs now systemd so we can test daemon related things

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -3,10 +3,12 @@
 parent_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")"; cd ..; pwd -P )
 image_id=$(docker buildx build -q --load $parent_dir)
 
-docker run --rm -i $image_id
+container_id=$(docker run --rm -d --privileged=true $image_id)
+
+docker exec -i $container_id go test ./...
 exit_code=$(echo $?)
 
-docker rmi $image_id
+docker stop $container_id
+docker rmi -f $image_id
 
 exit $exit_code
-
