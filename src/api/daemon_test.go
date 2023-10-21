afcf5b3f030d58e427e935d7b9e43212da423876
cooligus <pewienjaaa@gmail.com>
2023-10-21 11:39:26 +0200

    and GitHub actions

--- src/api/daemon_test.go
+++ src/api/daemon_test.go
@@ -1,13 +1,14 @@
 package api
 
-import (
-	"testing"
-)
-
-func TestGetDaemon(t *testing.T) {
-	//daemonInfo, err := GetDaemon("systemctl", "systemd-homed")
-
-	//if daemonInfo.name == "" || daemonInfo.isActive == unknown || daemonInfo.isEnabled == unknown {
-	//	t.Fatalf("Error occured when obtaining daemon data: %s", err)
-	//}
-}
+//
+//import (
+//	"testing"
+//)
+//
+//func TestGetDaemon(t *testing.T) {
+//	//daemonInfo, err := GetDaemon("systemctl", "systemd-homed")
+//
+//	//if daemonInfo.name == "" || daemonInfo.isActive == unknown || daemonInfo.isEnabled == unknown {
+//	//	t.Fatalf("Error occured when obtaining daemon data: %s", err)
+//	//}
+//}
