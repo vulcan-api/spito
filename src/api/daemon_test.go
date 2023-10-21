9847460d7b4ae5dbc030cad0b7cd38e029e939bd
cooligus <pewienjaaa@gmail.com>
2023-10-21 11:43:16 +0200

    That's what I like

--- src/api/daemon_test.go
+++ src/api/daemon_test.go
@@ -1,14 +1,11 @@
 package api
 
-//
-//import (
-//	"testing"
-//)
-//
-//func TestGetDaemon(t *testing.T) {
-//	//daemonInfo, err := GetDaemon("systemctl", "systemd-homed")
-//
-//	//if daemonInfo.name == "" || daemonInfo.isActive == unknown || daemonInfo.isEnabled == unknown {
-//	//	t.Fatalf("Error occured when obtaining daemon data: %s", err)
-//	//}
-//}
+import "testing"
+
+func TestGetDaemon(t *testing.T) {
+	//daemonInfo, err := GetDaemon("systemctl", "systemd-homed")
+
+	//if daemonInfo.name == "" || daemonInfo.isActive == unknown || daemonInfo.isEnabled == unknown {
+	//	t.Fatalf("Error occured when obtaining daemon data: %s", err)
+	//}
+}
