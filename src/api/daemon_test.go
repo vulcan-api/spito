fa6433fa8587dc0dfa3bef5b8cf5f2db890a2734
cooligus <pewienjaaa@gmail.com>
2023-10-21 11:48:26 +0200

    .

--- src/api/daemon_test.go
+++ src/api/daemon_test.go
@@ -3,7 +3,7 @@ package api
 import "testing"
 
 func TestGetDaemon(t *testing.T) {
-	daemonInfo, err := GetDaemon("systemctl", "systemd-homed")
+	daemonInfo, err := GetDaemon("systemctl", "dbus")
 
 	if daemonInfo.name == "" || daemonInfo.isActive == unknown || daemonInfo.isEnabled == unknown {
 		t.Fatalf("Error occured when obtaining daemon data: %s", err)
