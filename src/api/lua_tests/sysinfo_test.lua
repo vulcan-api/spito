f9aef2b82d4a9d149ce87388a9bee6d9241e31bc
cooligus <pewienjaaa@gmail.com>
2023-11-14 13:57:08 +0100

    Fix broken tests

--- src/api/lua_tests/sysinfo_test.lua
+++ src/api/lua_tests/sysinfo_test.lua
@@ -1,5 +1,6 @@
 function main()
-	 initSystem = GetInitSystem()
+	 initSystem, err = GetInitSystem()
+	 if err ~= nil then return false end
 	 distro = GetDistro()
-	 return initSystem ~= "" and distro.Version~= "" and distro.Name ~= ""
+	 return initSystem ~= "" and distro.Name ~= ""
 end
