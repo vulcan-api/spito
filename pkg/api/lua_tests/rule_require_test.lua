0c1a2f0946be26d6e18d03adaec8b4cdd685a9d7
sewe2000 <archdevx7d6@sewe-hub.xyz>
2024-01-06 16:48:47 +0100

    Expand a test

--- pkg/api/lua_tests/rule_require_test.lua
+++ pkg/api/lua_tests/rule_require_test.lua
@@ -1,3 +1,5 @@
 function main()
-    return require_remote("https://github.com/avorty/spito-ruleset", "dbus")
+    require_remote_result = require_remote("https://github.com/avorty/spito-ruleset", "dbus")
+    require_result = require_file('./daemon_test.lua')
+    return require_remote_result and require_result
 end
