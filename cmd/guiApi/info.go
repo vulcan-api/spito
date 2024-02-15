e9769c6462debcd9d77bfd8ab5825f996b597530
cooligus <pewienjaaa@gmail.com>
2024-02-15 07:24:39 +0100

    Fix broken dbus connection with gui

--- cmd/guiApi/info.go
+++ cmd/guiApi/info.go
@@ -3,6 +3,7 @@ package guiApi
 import (
 	"fmt"
 	"github.com/godbus/dbus"
+	"os"
 )
 
 type InfoApi struct {
@@ -32,7 +33,6 @@ func (i InfoApi) Important(args ...any) {
 // Most of the time we ignore potential error because it is not really important
 // and our app can work even if error is thrown
 func sendToDbusMethod(busObject dbus.BusObject, logType string, values ...any) error {
-	mergedValues := fmt.Sprint(values...)
-	call := busObject.Call("Echo", 0, logType, mergedValues)
+	call := busObject.Call(os.Getenv("DBUS_INTERFACE_ID")+".Info", 0, logType, fmt.Sprint(values...))
 	return call.Err
 }
