0b81cbe275098a074a31514b434a16e9d6e28870
sewe2000 <archdevx7d6@sewe-hub.xyz>
2024-02-25 19:08:39 +0100

    Hopefully fix some tests

--- internal/lua_tests/daemon_test.lua
+++ internal/lua_tests/daemon_test.lua
@@ -1,7 +1,7 @@
 function main()
     
     local dbus = api.sys.getDaemon("dbus")
-    if dbus.Name == "" or not dbus.IsActive or not dbus.IsEnabled then
+    if dbus == nil or dbus.Name == "" or not dbus.IsActive or not dbus.IsEnabled then
         return false
     end
     return true
